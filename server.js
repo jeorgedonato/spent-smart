f21713ed72e38cee6c4a16d9b29c5715c8c5e59a
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-02 20:51:36 -0800

    Added Auth and Income Controller along with the config db

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ extended: false }));
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 
 //Define routes here
+app.use('/api/auth', require('./controllers/authController'));
+app.use('/api/income', require('./controllers/incomeController'));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
