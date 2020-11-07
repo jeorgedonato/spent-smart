96dd7c71ac747bf8e95855a5d5ab841bc93738b0
Lauren Goettsch <lgoettsch2181@gmail.com>
2020-11-07 10:52:48 -0800

    login-page-logo

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,33 +1,25 @@
 import React from "react";
 import "./style.css";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-function SearchForm(props) {
-  return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="breed">Breed Name:</label>
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
-          type="text"
-          className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
-        />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-        </button>
-      </div>
-    </form>
-  );
-}
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      Login Here!
+    </Form.Text>
+  </Form.Group>
 
-export default SearchForm;
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+export default LoginForm;
