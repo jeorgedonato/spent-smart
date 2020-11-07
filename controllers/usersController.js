9e1f20e4b611d7b24cec3d90d727e697d99af8c7
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-07 13:27:07 -0800

    Added Email Confirmation Endpoint

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,42 +1,109 @@
 // Dependencies
 const db = require('../models');
-// const auth = require('../middleware/auth');
+const auth = require('../middleware/auth');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const router = require('./expenseController');
+const nodemailer = require("nodemailer");
 
 // Register User
 // Route : /api/users
-module.exports = function (app) {
-    app.post('/api/users', async function (req, res) {
-        const { username, password, email, firstname, lastname, cpassword } = req.body;
+router.post('/register', async (req, res) => {
+    const { email, firstname, lastname, password } = req.body;
+    try {
+        let user = await db.User.findOne({ email });
 
-        if (password !== cpassword) {
-            return res.status(400).json("Passwords didn't match");
+        if (user) {
+            return res
+                .status(400)
+                .json({ errors: [{ msg: 'User already exists' }] });
         }
 
-        try {
-            let user = await db.User.findOne({ where: { username: username } });
-            if (user) {
-                return res.status(400).json('User already exists');
-            }
-            const salt = await bcrypt.genSalt(10);
-            const hashedP = await bcrypt.hash(password, salt);
-            let createdUser = await db.User.create({ username, email, password: hashedP, firstname, lastname, user_money: 10000 });
+        user = new db.User({
+            email,
+            firstname,
+            lastname,
+            password
+        });
 
-            const payload = {
-                user: {
-                    id: createdUser.dataValues.id
-                }
-            }
+        const salt = await bcrypt.genSalt(10);
 
-            jwt.sign(payload, config.get('jwtSecretKey'), { expiresIn: 3600 }, function (err, token) {
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const emailConUrl = req.protocol + '://' + req.get('host') + '/api/users/confirm/' + user._id;
+        // console.log(emailConUrl)
+
+        let transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: config.get("emailHost"), // generated ethereal user
+                pass: config.get("emailPassword"), // generated ethereal password
+            }
+        });
+
+        await transporter.sendMail({
+            from: config.get("emailHost"), // sender address
+            to: user.email, // list of receivers
+            subject: "Account Confirmation for Spent Smart", // Subject line
+            text: `Click this link to confirm your accout `, // plain text body
+            html: `<b>Click this link to confirm you account : <u>${emailConUrl}</u></b>`, // html body
+        });
+
+        res.status(200).send("Please check your email for the Account Confirmation");
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+router.get('/confirm/:id', async (req, res) => {
+    // const confirmId = req.params.id;
+
+    try {
+        let user = await db.User.findById(req.params.id);
+        // console.log(user)
+         if (!user) {
+            return res
+                .status(400)
+                .json({ errors: [{ msg: "User does not exist" }] });
+        }
+
+        if(user.email_confirm){
+            return res
+                .status(201)
+                .json({ errors: [{ msg: "User is already confirmed" }] });
+        }
+
+    
+        user.email_confirm = true;
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
                 if (err) throw err;
                 res.json({ token });
-            });
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }
-    });
-};
+            }
+        );
+
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 const connectDB = require('./config/db');
 
 connectDB();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 // Serve up static assets (usually on heroku)
 
 
 //Define routes here
+app.use('/api/users', require('./controllers/usersController'));
 app.use('/api/auth', require('./controllers/authController'));
 app.use('/api/income', require('./controllers/incomeController'));
-app.use('/api/users', require('./controllers/usersController'));
 app.use('/api/categories', require('./controllers/categoriesController'));
 app.use('/api/expense', require('./controllers/expenseController'));
 
@@ -29,5 +28,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(PORT, () => {
+  console.log(PORT)
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
