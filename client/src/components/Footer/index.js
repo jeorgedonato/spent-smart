ea39ab0e5a78c49f607473304cf4449dfc2dd340
AmirAshkan7091 <amirashsd@yahoo.com>
2020-11-16 20:45:10 -0800

    amir

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,12 +4,11 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-         <div className="span" >
-            <span ><a href="/" taget="_self"className="footer-link">© 2020 Created by AmirAshkan S.Dardashti, Jeorge Donato, Lauren Goettsch, Duyen Pham and Raymond Tieu.  </a></span>
-         </div>
-      {/* <span>© </span> */}
-    </footer>
+   <footer className="footer">
+   <div className="span" >
+      <span ><a href="/" taget="_self"className="footer-link">© 2020 Created by AmirAshkan S.Dardashti, Jeorge Donato, Lauren Goettsch, Duyen Pham and Raymond Tieu.  </a></span>
+   </div>
+</footer>
   );
 }
 
