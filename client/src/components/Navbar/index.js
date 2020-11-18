cd557935294be728242197c36dd2281349d1aa00
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-17 19:17:20 -0800

    Added add and delete expenses

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -61,7 +61,7 @@ function Navbar({ auth: { isAuthenticated, loading }, logout }) {
           [
             <li key="0"><Link to="/expenses">Expenses</Link></li>,
             <li key="1"><Link to="/incomes">Incomes</Link></li>,
-            <li key="2"><Link to="/register">Logout</Link></li>
+            <li key="2"><Link to="" onClick={logout}>Logout</Link></li>
           ] :
           [
             <li key="0"><Link to="/about">About</Link></li>,
