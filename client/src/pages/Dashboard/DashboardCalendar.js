b33d5012bc2003b952d111cd5e696c5794ef2f52
Jeorge Donato <jeorgekhenr@gmail.com>
2020-12-03 20:56:11 -0800

    modified-calendar

--- client/src/pages/Dashboard/DashboardCalendar.js
+++ client/src/pages/Dashboard/DashboardCalendar.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"; 
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {getIncomes} from '../../actions/incomes';
+import {getExpenses} from '../../actions/expenses';
+import moment from 'moment';
 
-const DashboardCalendar = () => {
+
+const DashboardCalendar = ({incomes, expenses , getExpenses, getIncomes}) => {
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    getIncomes()
+    getExpenses()
+    expenses.map(expenses => {
+      renderEventContent(expenses)
+    })
+
+  },[getExpenses,getIncomes])
+
+  const handleDateClick = arg => {
+    console.log(arg);
+  }
     return (
         <>
         <FullCalendar
-        plugins={[ dayGridPlugin ]}
+        plugins={[ dayGridPlugin, interactionPlugin ]}
         initialView="dayGridMonth"
+        dateClick={handleDateClick}
       />
         </>
     );
 };
 
+DashboardCalendar.propTypes = {
+  incomes: PropTypes.array.isRequired,
+  expenses: PropTypes.array.isRequired,
+  getExpenses : PropTypes.func.isRequired,
+  getIncomes : PropTypes.func.isRequired,
+}
 
-export default DashboardCalendar;
+const mapStateToProps = state => ({
+  incomes: state.incomes.incomes,
+  expenses: state.expenses.expenses,
+})
+
+
+export default connect(mapStateToProps,{getExpenses, getIncomes})(DashboardCalendar);
